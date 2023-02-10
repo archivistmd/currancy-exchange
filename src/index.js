bb38a2f7bd24872d318ff16069d9e660228a8662
Mesha <madsketchz@gmail.com>
2023-02-10 09:02:06 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyExchange from './CurrencyExchange';
+
+// Business logic
+
+
+// UI logic
+
+function displayConversion(response, amount, fromCurr, toCurr) {
+  let div = document.getElementById("display-result");
+  div.innerText = null;
+  div.innerText = `${amount} ${fromCurr} = ${response.conversion_result} ${toCurr}`;
+}
+
+function displayError(response) {
+  document.getElementById("display-result").innerText = `Apologies, there was an error in converting your currency: ${response.result}: ${response.errortype}`;
+}
+
+function formSubmission(event) {
+  event.preventDefault();
+  const amount = document.getElementById("conversion-amount").value;
+  const fromCurr = document.getElementById("starting-unit").value;
+  const toCurr = document.getElementById("converting-to").value;
+  getConversion(amount, fromCurr, toCurr);
+  document.getElementById("converter-form").reset();
+}
+
+document.getElementById("converter-form").addEventListener("submit", formSubmission);
