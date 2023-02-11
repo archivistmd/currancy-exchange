cd044ed215e1d0bebec00d9c54949a3b691b54a6
Mesha <madsketchz@gmail.com>
2023-02-10 16:40:51 -0800

    Update CurrencyExchange.js, remove errors and unnecessary code

--- src/CurrencyExchange.js
+++ src/CurrencyExchange.js
@@ -3,17 +3,17 @@ export default class CurrencyExchange {
   static async getConversion(amount, fromCurr, toCurr) {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${fromCurr}/${toCurr}/${amount}`);
-      const jsonResponse = await response.json();
+      // const jsonResponse = await response.json();
       if (!response.ok) {
         console.log(response);
         const errorMessage = `${response.status}`;
-        throw new Error(errorMessage)
+        throw new Error(errorMessage);
       } else {
         return response.json();
       }
-      return jsonResponse;
+      // return jsonResponse;
     } catch(error) {
       return error;
-    };
+    }
   }
 }
